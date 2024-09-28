2a806e6336acd3d9eed228c553fd7e00ce619cdb
mohammedjavadkt <javadkt@gmail.com>
2024-09-28 13:09:43 +0400

    React native dev tool debugger lib

--- App.tsx
+++ App.tsx
@@ -24,6 +24,7 @@ import {
   LearnMoreLinks,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
+import "react-native-devsettings";
 
 type SectionProps = PropsWithChildren<{
   title: string;
